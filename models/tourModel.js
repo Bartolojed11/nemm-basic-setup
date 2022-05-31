56781f0d5c3e6de92e1245ac759f83bbd555aed0
jbartolo <jbartolo@cloudpanda.ph>
2022-05-31 20:49:29 +0800

    Mongoose Document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -10,6 +10,9 @@ const TourSchema = mongoose.Schema({
     required: [true, 'A Tour must have a name'],
     unique: true,
   },
+  slug: {
+    type: String,
+  },
   rating: {
     type: Number,
     default: 4.5,
@@ -37,11 +40,11 @@ const TourSchema = mongoose.Schema({
     type: Number,
     default: 0,
   },
-  summary : {
+  summary: {
     type: String,
     required: true,
   },
-  description : {
+  description: {
     type: String,
     required: true,
   },
@@ -50,20 +53,36 @@ const TourSchema = mongoose.Schema({
     required: true,
   },
   startDates: {
-      required: true,
-      type: [Date],
+    required: true,
+    type: [Date],
   }
 }, {
   // Means it should show the virtuals when having a json and object result
-  toJSON: { virtuals: true},
-  toObject: { virtuals: true},
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
 });
 
 // virtual is a bit like the view on MySql Database
 // Virtual can't be queried, (eg) Tour.find().where('durationWeeks', 1)
-TourSchema.virtual('durationWeeks').get(function() {
+TourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 })
+
+/**
+ * Document Middleware start : runs before/after of .save() and .create() but not on .insertMany()
+ * Document Middleware are like Observers in Laravel
+ * 'save' is called a hook
+ */
+TourSchema.pre('save', function (next) {
+  console.log(this)
+  this.slug = slugify(this.name, { lower: true })
+  next()
+})
+
+TourSchema.post('save', function (doc, next) {
+  next()
+})
+// Document Middleware end
 
 const Tour = mongoose.model('Tour', TourSchema);
 
