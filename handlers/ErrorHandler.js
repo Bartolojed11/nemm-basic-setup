8a8a8dc8869326c8a6ab055b203d507c22c41b7a
jbartolo <jbartolo@cloudpanda.ph>
2022-06-03 00:27:14 +0800

    development and production error handlers and handling CastError

--- handlers/ErrorHandler.js
+++ handlers/ErrorHandler.js
@@ -1,8 +1,48 @@
-const Response = require('./../helpers/Response')
+const AppError = require('./../handlers/AppError')
+
+const sendDevErrors = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    })
+}
+
+const sendProdErrors = (err, res) => {
+    // Operational error
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        })
+    } else {
+        // Programming or other unknown error
+        console.error('ERROR', err)
+        res.status(500).json({
+            'status': 'error',
+            'message': 'Something went wrong!'
+        })
+    }
+}
+
+const handleCastErrorDB = (err) => {
+    // path: where the error occurred
+    // value: the value that cause the error
+    const message = `Invalid ${err.path}: ${err.value}`
+    return new AppError(message, 400)
+}
 
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500
     err.status = err.status || 'error'
-    console.log(err.stack)
-    return new Response(res, err.statusCode, err.message, err.status)
+    if (process.env.NODE_ENV === 'production') {
+        let error = { ...err }
+
+        if (err.name === 'CastError') error = handleCastErrorDB(error)
+
+        sendProdErrors(error, res)
+    } else {
+        sendDevErrors(err, res)
+    }
 }
