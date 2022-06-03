002f034e4a1fce866a5e83f612abe532094eebb2
jbartolo <jbartolo@cloudpanda.ph>
2022-06-03 21:51:51 +0800

    DB validation error handler and global handlers for unhandled rejections

--- handlers/ErrorHandler.js
+++ handlers/ErrorHandler.js
@@ -32,11 +32,19 @@ const handleCastErrorDB = (err) => {
     const message = `Invalid ${err.path}: ${err.value}`
     return new AppError(message, 400)
 }
+
 const handleDuplicateDB = (err) => {
     // path: where the error occurred
     // value: the value that cause the error
     const value = err.message.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0]
     const message = `Duplicate field value :  ${value}`
+    return new AppError(message, 400)
+}
+
+const handleValidationErrorDB = (err) => {
+    const errors = Object.values(err.errors).map(el => el.message)
+
+    const message = `Invalid input data. ${errors.join('. ')}`
     return new AppError(message, 400)
 }
 
@@ -51,6 +59,7 @@ module.exports = (err, req, res, next) => {
 
         if (err.name === 'CastError') error = handleCastErrorDB(err)
         if (err.code === 11000) error = handleDuplicateDB(err)
+        if (err.name === 'ValidationError') error = handleValidationErrorDB(err)
 
         sendProdErrors(error, res)
     } else {
--- server.js
+++ server.js
@@ -24,7 +24,18 @@ mongoose.connect(process.env.DATABASE, {
 
 const port = process.env.PORT
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}`)
 })
 
+// This will handle all unhandled exceptions (eg) Async without catch
+process.on('unhandledRejection', (err) => {
+    console.log(err.name, err.message)
+    console.log('UNHANDLED EXCEPTION')
+    // Close the server first before exit()
+    // This way, it lets the server finish all the requests before shutting down
+    server.close(() => {
+        // 1 is for unhandled rejection
+        process.exit(1)
+    })
+})
